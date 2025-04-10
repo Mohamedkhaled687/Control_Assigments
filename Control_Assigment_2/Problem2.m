21e1dd09b5ced3adb786051f63d4fdbe65146320
Mohamedkhaled687 <mohamedkhaled47526@gmail.com>
2025-04-10 21:21:06 +0200

    last commit

--- Control_Assigment_2/Problem2.m
+++ Control_Assigment_2/Problem2.m
@@ -1,28 +1,30 @@
-% Problem2.m
-% This script simulates the step response of a system defined by
-% C(s)/R(s) = 4 / (s^2 + 6s + 4) for a unit step input.
-% It computes the rise time, maximum overshoot, and settling time.
+% step_response_simulation.m
+% This script simulates the step response of a system defined by the
+% transfer function C(s)/R(s) = 4 / (s^2 + 6s + 4) for a unit step input.
+% It computes performance metrics such as rise time, maximum overshoot,
+% and settling time.
 
 clc;            % Clear the command window
 clear;          % Clear workspace variables
 close all;      % Close all open figure windows
 
 %% Define the Transfer Function
-num = 4;                 % Numerator coefficient
-den = [1 6 4];           % Denominator coefficients for s^2 + 6s + 4
-sys = tf(num, den);      % Create the transfer function model
+% Numerator and denominator of the transfer function:
+% C(s)/R(s) = 4 / (s^2 + 6s + 4)
+num = 4;                    % Numerator coefficient (constant gain)
+den = [1 6 4];              % Denominator polynomial coefficients for s^2 + 6s + 4
+sys = tf(num, den);         % Create the transfer function model
 
 %% Simulate the Step Response
 figure('Name','Unit Step Response');
-step(sys);   % Plot the step response using the default style
+step(sys, 'LineWidth',2);   % Plot the step response using a unit step input
 grid on;
 title('Step Response for C(s)/R(s) = 4 / (s^2 + 6s + 4)');
 xlabel('Time (s)');
 ylabel('System Response');
 
-
 %% Compute Performance Metrics
-% The stepinfo function calculates time-domain characteristics.
+% The stepinfo function computes several time-domain characteristics.
 info = stepinfo(sys);
 
 % Display the computed metrics:
